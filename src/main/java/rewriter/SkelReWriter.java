1bfe2b414571aaf76c2da0b4999af76b29d4f520
me <me@10.101.27.233>
2019-05-28 23:39:22 +0200

    refactor

--- src/main/java/rewriter/SkelReWriter.java
+++ src/main/java/rewriter/SkelReWriter.java
@@ -1,6 +1,7 @@
 package rewriter;
 
 import java.util.ArrayList;
+import java.util.stream.Collectors;
 
 import tree.model.CompPatt;
 import tree.model.FarmPatt;
@@ -9,6 +10,21 @@ import tree.model.PipePatt;
 import tree.model.SeqPatt;
 import tree.model.SkeletonPatt;
 
+/**
+ * pipeintro comp(D1;D2)! pipe(D1;D2)
+pipeelim pipe(D1;D2)!comp(D1;D2)
+compassoc comp(D1;comp(D2;D3))$comp(comp(D1;D2);D3)
+pipeassoc pipe(D1; pipe(D2;D3))$ pipe(pipe(D1;D2);D3)
+mapofcomp comp(map(D1);map(D2))!map(comp((D1;D2))
+compofmap map(comp(D1;D2)!comp((map(D1);map(D2))
+mapofpipe pipe(map(D1);map(D2))!map(pipe((D1;D2))
+pipeofmap map(pipe(D1;D2)! pipe((map(D1);map(D2))
+mapelim map(D)!D
+farmelim f arm(D)!D
+farmintro D! f arm(D)
+ * @author me
+ *
+ */
 public class SkelReWriter implements ReWriter {
 	private int n = 4;
 
@@ -55,10 +71,53 @@ public class SkelReWriter implements ReWriter {
 
 	@Override
 	public void reWrite(PipePatt s) {
+		ArrayList<SkeletonPatt> patterns = new ArrayList<>();
+
 		// pipe elim
+//		pipe(D1;D2) = comp(D1;D2)
+		CompPatt comp = new CompPatt("comp",( s.getChildren() != null? s.getChildren().stream().mapToLong(SkeletonPatt::serviceTime)
+				.reduce(0,(c1,c2) -> c1 + c2 ):0));
+		comp.setChildren(s.getChildren());
+		patterns.add(comp);
+		
+//		pipeassoc pipe(D1; pipe(D2;D3)) = pipe(pipe(D1;D2);D3)
+		if(s.getChildren() != null && s.getChildren().size() > 2) {
+			if(s.getChildren().get(0) instanceof PipePatt) {
+				PipePatt p0 = new PipePatt();
+			}
+			//find index of pipe
+			// if at 0, 
+				//get first child of pipe0 as patt0
+				// create pipe (patt0, pipe of the remaining patterns),
+			//if index > 0
+				// create pipe (patt@0, .. 1st element of inner pipe), remaining patterns)
+		}
 		// farm intro
+		FarmPatt farm = new FarmPatt("farm", s.getServiceTime()/n);
+		farm.setChild(s);
+		patterns.add(farm);
+		
+		//mapofpipe pipe(map(D1);map(D2))= map(pipe((D1;D2))
+		//if pipe has map stages 
+			//remove the map as map.getchild
+			//create pipe of map.getchild for all
+			// creat map of the pipe
+		if(s.getChildren().stream().allMatch(sk -> sk instanceof MapPatt)) {
+			
+			ArrayList<SkeletonPatt> listOfChildrens = (ArrayList<SkeletonPatt>) s.getChildren().stream().
+					map(p -> p.getChild()).collect(Collectors.toList());
+			MapPatt map = new MapPatt("map", s.getServiceTime()/n);
+			
+			PipePatt piMap = new PipePatt(s.getLable(),listOfChildrens.stream().mapToLong(SkeletonPatt::serviceTime)
+					.reduce(0,(c1,c2) -> c1 > c2 ? c1: c2 ) );
+			
+			piMap.setChildren(listOfChildrens);
+			map.setChild(piMap);
+			patterns.add(map);
+			}
 		// for each stage
 		// rewrite
+		s.setPatterns(patterns);
 	}
 
 	@Override
